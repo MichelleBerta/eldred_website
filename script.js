d3fe2b72e2b84610af524d746913e973c0ba48a1
Michelle Berta <bertafamily@sbcglobal.net>
2024-03-25 18:19:13 -0500

    swiper 1 wokring and formatted, changed background

--- script.js
+++ script.js
@@ -6,8 +6,14 @@ $(document).ready(function () {
 
 new Swiper("#swiper-1", {
   effect: "fade",
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
   pagination: {
     el: "#swiper-1 .swiper-pagination",
     clickable: true,
   },
+  lazyLoading: true,
+  loop: true,
 });
