654d61914bcfd9b865035284d9788dadb62666fc
Michelle Berta <bertafamily@sbcglobal.net>
2024-03-25 12:10:57 -0500

    began installing swiper

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ $(document).ready(function () {
   $(".collapsible").collapsible();
   $(".carousel").carousel();
 });
+
+new Swiper("#swiper-1", {
+  effect: "fade",
+  pagination: {
+    el: "#swiper-1 .swiper-pagination",
+    clickable: true,
+  },
+});
